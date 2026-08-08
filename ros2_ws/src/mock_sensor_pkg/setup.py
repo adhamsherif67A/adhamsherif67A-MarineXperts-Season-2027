259cf584b781786604e99d5d45dadb138ea0ffbb
ibrahimamr736-cyper <ibrahimrov736@gmail.com>
2026-08-08 12:57:14 +0300

    updated with TF2 task

---
+++ ros2_ws/src/mock_sensor_pkg/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'mock_sensor_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+      
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ibrahim',
+    maintainer_email='ibrahim@todo.todo',
+    description='Mock sensor tf2 broadcaster for drill',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+   
+            'broadcaster_node = mock_sensor_pkg.mock_broadcaster:main',
+        ],
+    },
+)
